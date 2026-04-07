d433e6cdf830a8085cd1f5acbeab02a1fe75d43d
Ishan Satya Prakash <prakashishan7540@gmail.com>
2026-04-07 23:19:31 +0530

    video 97

---
+++ chai_code/section-19/todo/todo.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const { takeCoverage } = require('v8');
+const filePath = "./tasks.json";
+
+const loadTasks = () => {
+    try {
+        const dataBuffer = fs.readFileSync(filePath);
+        const dataJson = dataBuffer.toString();
+        return JSON.parse(dataJson);
+    } catch (error) {
+        return []; // important fix
+    }
+}
+
+
+
+const saveTasks = (tasks) => {
+    const dataJSON = JSON.stringify(tasks);
+    fs.writeFileSync(filePath, dataJSON);
+}
+
+const addTask = (task) => {
+    const tasks = loadTasks();
+    tasks.push({ task });
+    saveTasks(tasks);
+    console.log("Task added:", task);
+}
+
+const listTasks = () => {
+    const tasks = loadTasks();
+    for (x in tasks) {
+        console.log(tasks[x]);
+    }
+}
+
+const removeTasks = (index) => {
+    const tasks = loadTasks();
+    const updated = tasks.filter((_, i) => i !== index);
+    saveTasks(updated);
+    console.log("Task removed");
+}
+
+const command = process.argv[2];
+const argument = process.argv[3];
+
+if (command === 'add') {
+    addTask(argument);
+} else if (command === 'list') {
+    listTasks();
+} else if (command === 'remove') {
+    removeTasks(parseInt(argument));
+} else {
+    console.log("Command not recognised");
+}
