9b35d895925d688910d88868558e31b2264aa8ed
Ishan Satya Prakash <prakashishan7540@gmail.com>
2026-04-17 12:30:49 +0530

    connect mongoDB

--- chai_code/section-21/src/index.js
+++ chai_code/section-21/src/index.js
@@ -1,12 +1,19 @@
 import dotenv from "dotenv";
 import app from "./app.js";
-
+import connectDB from "./db/index.js";
 dotenv.config({
   path: "./.env",
 });
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`listenting to port http://localhost:${port}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`listenting to port http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed", err);
+    process.exit(1);
+  });
