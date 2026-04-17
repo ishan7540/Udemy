4bee041550eb0222b273a3eddf4bfdc20d77aa50
Ishan Satya Prakash <prakashishan7540@gmail.com>
2026-04-17 18:18:45 +0530

    test

--- chai_code/section-21/src/models/user.model.js
+++ chai_code/section-21/src/models/user.model.js
@@ -60,4 +60,12 @@ const userSchemea = new Schema(
   },
 );
 
+userSchemea.pre("save", async function(next){
+    if(!this.isModified("password"))
+    this.password = bcrypt.hash(this.password,10)
+    next()
+})
+
+
 export const User = mongoose.model("User", userSchemea);
+.
