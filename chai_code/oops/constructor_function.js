a4e1b485a6e7a3cf50af5a3aaa7b4e2f5f31ed5e
Ishan Satya Prakash <prakashishan7540@gmail.com>
2026-04-22 22:51:58 +0530

    add : new model

---
+++ chai_code/oops/constructor_function.js
@@ -0,0 +1,14 @@
+function Person(name,age){
+    this.name = name;
+    this.age = age;   
+}
+
+function Car(make,mode){
+    this.make = make;
+    this.model = model;
+}
+
+let myCar = new Car("Toyota", "Camry");
+console.log(myCar);
+
+let 
